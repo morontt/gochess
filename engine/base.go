9721f4f3b6a53488f8bf9e26c6ac509e016fe31d
morontt <morontt@gmail.com>
2014-07-06 11:23:56 +0300

    add packages

---
+++ engine/base.go
@@ -0,0 +1,6 @@
+package engine
+
+func GoTest() string {
+    return "test package"
+}
+
---
+++ main/gochess.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "gochess/engine"
+)
+
+func main() {
+    f := engine.GoTest()
+
+    fmt.Println(f)
+}
+
